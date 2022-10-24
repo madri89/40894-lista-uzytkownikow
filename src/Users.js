860ca8320f1fa6794dea9c968301e8459a0a7e49
madri89 <marcinrossa@wp.pl>
2022-10-24 22:54:05 +0200

    gotowe

---
+++ src/Users.js
@@ -0,0 +1,66 @@
+import React, {Component} from 'react';
+import './Users.css';
+
+import UsersList from './UsersList'
+
+class Users extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            users: [],
+
+        error:false
+        };
+    }
+
+    addUser = (event) => {
+        event.preventDefault();
+
+        if (this._inputName.value.trim() === ''){
+            this.setState({error:true})
+            return
+
+            }
+
+        let newUser = {
+            id: Date.now(),
+            name: this._inputName.value
+        }
+
+        this.setState((state) => {
+            return ({
+                users: state.users.concat(newUser)
+            });
+        })
+
+        this._inputName.value = '';
+        this.setState({error: false})
+    }
+
+    removeUser = (userID) => {
+        this.setState(state => {
+            return({
+                users: state.users.filter((user) => {return(user.id !== userID)})
+            });
+        })
+    }
+
+        render() {
+            return(
+                <div className="users-main">
+                    <h1>User`s List</h1>
+                    <form onSubmit={this.addUser}>
+                        <input className={this.state.error?"input-error":''} ref={(element) => {this._inputName = element;}} type="text" placeholder="Enter name"/>
+                        <button type="submit">Add user</button>
+                    </form>
+                    <UsersList usersList={this.state.users} removeUserMethod={this.removeUser}/>
+                </div>
+            );
+        }
+
+}
+
+export default Users;
+  
